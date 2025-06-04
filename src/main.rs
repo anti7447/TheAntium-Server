341d9b043ef451c4160a815862bb18cb422d7e0e
Siegxc <siegxc@TheGreatThresher>
2025-06-04 16:39:55 +0300

    что-то сделал

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+  HttpResponse::Ok().body("The Antium Server")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+  HttpResponse::Ok().body(req_body)
+}
+
+#[get("/users")]
+async fn users() -> impl Responder {
+  HttpResponse::Ok().body("Юзверьc")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .service(hello)
+      .service(echo)
+      .service(users)
+  })
+  .bind(("127.0.0.1", 8080))?
+  .run()
+  .await
 }
